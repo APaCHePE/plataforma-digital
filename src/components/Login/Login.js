6dee049f535300a850caf06c108f1a74e88b97db
Jackeline Cardenas <jackelinel.cardenasgavino@gmail.com>
2021-10-02 01:07:32 -0500

    correccion de login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,21 +2,25 @@ import React from 'react'
 import Button from '@material-ui/core/Button';
 import { red } from '@material-ui/core/colors';
 import Input from '@mui/material/Input';
+import './Login.scss';
 
 export const Login = () => {
     return (
+        <>
         <div>
-            <div style={{background:red}}>
-                <h3>SAVE</h3>
-                <h6>Servicio de atención vecinal</h6>
-                <h6>Usuario</h6>
+            <div className="color-de-fondo">
+                <h3 className="color-text">SAVE</h3>
+                <h6 className="color-text">Servicio de atención vecinal</h6>
+                <h6 className="color-text">Usuario</h6>
                 <Input id="component-simple" />
-                <h6>Contraseña</h6>
+                <h6 className="color-text">Contraseña</h6>
                 <Input id="component-simple" />
-            <Button variant="contained" color="primary">
+                <br/><br/>
+                <Button variant="contained" color="primary">
                 Ingresar
             </Button> 
             </div>
         </div>
+    </>
     )
 }
