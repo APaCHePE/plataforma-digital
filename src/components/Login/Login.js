e8b77243b73ccad06ef4c5c28ea4b7daf72ec2c7
Renzo Elias <renzoeliasdelacalle@gmail.com>
2021-10-24 02:56:39 -0500

    login register styled

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
-import Input from "@mui/material/Input";
+import TextField from '@mui/material/TextField';
 import "./Login.scss";
 
 export const Login = () => {
+  const [user, setUser] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleChangeSelect = ((event, set) => {
+    set(event.target.value);
+  });
+
   return (
     <>
       <div className="App">
@@ -12,19 +19,27 @@ export const Login = () => {
           <img src="https://www.miraflores.gob.pe/modulos/intranet/dist/logomuniwhite.png?d9153b5383f7b03e9060a26be639f28f" />
           <div className="color-de-fondo">
             <p className="color-text-titulo">SAVE</p>
-            <h6 className="color-text subtitulo">
+            <p className="color-text-subtitulo">
               Servicio de atención vecinal
-            </h6>
-            <Input placeholder="Usuario" id="component-simple" />
-            <br />
-            <br />
-            <Input
-              placeholder="Contraseña"
-              type="password"
-              id="component-simple"
-            />
-            <br />
-            <br />
+            </p>
+            <div className="item_login">
+              <p className="">Usuario</p>
+              <TextField
+                id="demo-helper-text-aligned"
+                value={user}
+                onChange={(e) => handleChangeSelect(e, setUser)}
+                placeholder=""
+              />
+            </div>
+            <div className="item_login">
+              <p className="">Contraseña</p>
+              <TextField
+                id="demo-helper-text-aligned"
+                value={password}
+                onChange={(e) => handleChangeSelect(e, setPassword)}
+                placeholder=""
+              />
+            </div>
             <Link to="/register">
               <Button
                 className="button-login"
--- src/components/RegistroSave/RegistroSave.js
+++ src/components/RegistroSave/RegistroSave.js
@@ -1,47 +1,109 @@
 import React from "react";
 import "./RegistroSave.scss";
-import Input from "@mui/material/Input";
-
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Button from "@material-ui/core/Button";
+
 
 export const RegistroSave = () => {
   const [age, setAge] = React.useState('');
+  const [name, setName] = React.useState('');
 
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  };
+  const [value, setValue] = React.useState('');
+
+  const handleChangeSelect = ((event, set) => {
+    set(event.target.value);
+  });
+
   return (
     <>
       <div className="container">
 
-        <div className="titulo-principal">Cuentanos tu problema</div>
+        <div className="titulo-principal">
+          Cuentanos tu problema
+          <img src="https://www.miraflores.gob.pe/wp-content/themes/munimiraflores/web-imagenes/destacado-default.png" />
+        </div>
         <div className="cuerpo-modal">
-            <div>
-              <p className="texto-izquierda">Tipo de problema</p>
-              <Input />
+            <div className="item_1">
+              <div className="item_1_left">
+                <p className="texto-izquierda">Tipo de problema</p>
+                <Select
+                  value={age}
+                  onChange={(e) => handleChangeSelect(e, setAge)}
+                >
+                  <MenuItem value={10}>Adulto Mayor</MenuItem>
+                  <MenuItem value={20}>Discotecas y Bares</MenuItem>
+                  <MenuItem value={30}>Tesoreria</MenuItem>
+                  <MenuItem value={40}>Fiscalización</MenuItem>
+                  <MenuItem value={50}>Limpieza de parques</MenuItem>
+                  {/* {names.map((name) => (
+                    <MenuItem
+                      key={name}
+                      value={name}
+                    >
+                      {name}
+                    </MenuItem>
+                  ))} */}
+                </Select>
+              </div>
+              <div className="item_1_right">
+                <p className="texto-derecha">Ubicación de ocurrencia</p>
+                <TextField
+                  id="demo-helper-text-aligned"
+                  value={name}
+                  onChange={(e) => handleChangeSelect(e, setName)}
+                  placeholder="Escriba una Av./Calle/Jr./Psje"
+                />
+              </div>
             </div>
-            <div>
-              <p className="texto-derecha">Ubicación de ocurrencia</p>
-              <Input />
+            <div className="item_2">
+              <p className="texto-derecha">Descripcion:</p>
+              <TextField
+                id="fullWidth"
+                placeholder="Escribir aqui"
+                multiline
+                rows={4}
+              />
             </div>
-            <div>
-              <InputLabel id="demo-simple-select-label">Age</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={age}
-                label="Age"
-                onChange={handleChange}
+            <div className="item_3">
+              <p className="texto-derecha">Medio de Respuesta</p>
+              <RadioGroup
+                aria-label="gender"
+                name="controlled-radio-buttons-group"
+                value={value}
+                onChange={(e) => handleChangeSelect(e, setValue)}
               >
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
-              </Select>
+                <FormControlLabel value="correo" control={<Radio />} label="Correo" />
+                <FormControlLabel value="telefono" control={<Radio />} label="Telefono" />
+                <FormControlLabel value="carta" control={<Radio />} label="Carta" />
+                <FormControlLabel value="no deseo que me respondan" control={<Radio />} label="No deseo que me respondan" />
+              </RadioGroup>
+            </div>
+            <div className="item_4">
+              <p className="texto-derecha">Adjuntar archivos (opcional):</p>
+              <p className="texto-derecha">Puedes subir un maximo de 20 MB en fotos videos y/o textos que ayuden a evidenciar tu reclamo</p>
+              <Button
+                className="button-subir"
+                variant="contained"
+                color="primary"
+              >
+                Sube tus archivos
+              </Button>
             </div>
         </div>
-
+        <div className="registerSave">
+          <Button
+            className="button-registrar"
+            variant="contained"
+            color="primary"
+          >
+            Registrar
+          </Button>
+        </div>
       </div>
     </>
   );
