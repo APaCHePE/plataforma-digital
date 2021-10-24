266cf70807e7bf1be28722f98f076ed35656ae49
Cardenas Jackeline <apps@ransa.net>
2021-10-23 21:35:49 -0500

    select

--- src/components/RegistroSave/RegistroSave.js
+++ src/components/RegistroSave/RegistroSave.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./RegistroSave.scss";
 import Input from "@mui/material/Input";
 
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+
 export const RegistroSave = () => {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
   return (
     <>
       <div className="container">
@@ -16,6 +25,20 @@ export const RegistroSave = () => {
             <div>
               <p className="texto-derecha">Ubicación de ocurrencia</p>
               <Input />
+            </div>
+            <div>
+              <InputLabel id="demo-simple-select-label">Age</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={age}
+                label="Age"
+                onChange={handleChange}
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
             </div>
         </div>
 
